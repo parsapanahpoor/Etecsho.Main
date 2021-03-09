c5166f9e1c8d2e95fdd15fec7986db736414f6e9
parsa panahpoor <parsapanahpoor@yahoo.com>
2021-03-09 23:34:33 +0330

    Initial ProductCategori Entites

---
+++ Etecsho.DataLayer/Entites/Product/ProductSelectedCategory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Etecsho.Models.Entites.Product
+{
+   public  class ProductSelectedCategory
+    {
+
+
+        public ProductSelectedCategory()
+        {
+
+        } 
+
+        [Key]
+        public int ProductSelectedCategoryID { get; set; }
+
+        public int ProductCategoryId { get; set; }
+        public int ProductID { get; set; }
+
+        #region Relations
+
+        public virtual ProductCategories ProductCategories { get; set; }
+
+        //public virtual Blog Blog { get; set; }
+        #endregion
+
+
+
+
+    }
+}
